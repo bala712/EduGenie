4c6d3d7f388f258fceecf85449b9d6fefe3b3929
BALAMURUGAN M <154044584+bala712@users.noreply.github.com>
2026-09-25 23:09:54 +0530

    Update learning_path.py

--- learning_path.py
+++ learning_path.py
@@ -32,7 +32,7 @@ Keep the explanation clear and student-friendly.
 """
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.5-flash lite",
             contents=prompt
         )
 
