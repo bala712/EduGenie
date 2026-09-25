6ab91c4caf8da9fe913c1c7f2018da8d2e83aaaf
BALAMURUGAN M <154044584+bala712@users.noreply.github.com>
2026-09-25 21:42:27 +0530

    Update summary_module.py

--- summary_module.py
+++ summary_module.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
-import google.generativeai as genai
+from google import genai
 
 load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=API_KEY)
 
 genai.configure(api_key=API_KEY)
 
