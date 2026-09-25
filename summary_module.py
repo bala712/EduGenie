97e9cb6882937f848e6963f250abe29f7f0f6f39
BALAMURUGAN M <154044584+bala712@users.noreply.github.com>
2026-09-25 22:58:23 +0530

    Update summary_module.py

--- summary_module.py
+++ summary_module.py
@@ -24,7 +24,7 @@ Text:
 """
 
         response = client.models.generate_content(
-            model="gemini-3.6-flash",
+            model="gemini-3.5-flash lite",
             contents=prompt
         )
 
